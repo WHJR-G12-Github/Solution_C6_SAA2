3319d2ffae1fde1b8b9f93c91cc1b37447a39c7b
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-21 11:41:22 +0530

    Create solution_c6_saa2.py

---
+++ solution_c6_saa2.py
@@ -0,0 +1,13 @@
+# Creating a list 'numbers'
+numbers=[1,2,0,4,6,8]
+# Creating a variable 'sum'
+# Initializing it to '0'
+sum=0
+
+# Creating a 'for' loop to access every element in the list
+for num in numbers:
+  # Add 'num' to 'sum'
+  sum=sum+num
+
+# Printing the value of 'sum'
+print("Sum:",sum)
